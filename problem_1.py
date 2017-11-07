822727f423d9f11add437e1147c361e3fb231422
Ryan Asensio <ryan.asensio@gmail.com>
2017-11-07 09:58:09 -0600

    solve problem 1 and benchmark two different solutions

--- problem_1.py
+++ problem_1.py
@@ -5,10 +5,25 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+import cProfile
 
-sum = 0
-for i in xrange(0, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
+def multiple_loop():
+    accum = 0
+    for i in xrange(0, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            accum += i
 
-print sum
+    return accum
+
+
+def multiple_set():
+    range_3 = xrange(0, 1000, 3)
+    range_5 = xrange(0, 1000, 5)
+    return sum(set(range_3).union(range_5))
+
+def test_func(func, n):
+    for _ in xrange(n):
+        func()
+
+cProfile.run("test_func(multiple_loop, 10000)") # 5x slower than using set
+cProfile.run("test_func(multiple_set, 10000)") # fastest solution
